1b1fbfecac7aeb4740f5ee8abd023a1b61dc08d4
Andrei Scutariu <andreiscutariu92@gmail.com>
2015-10-25 22:46:13 +0200

    second

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var app = express();
-var fs = require("fs"); 
+var express     =   require("express");
+var bodyParser  =   require("body-parser");
+var fs			= 	require("fs"); 
+var app         =   express();
 
+//Repository
 var usersRepository = {
 	getAll: function (cbk) {
 		fs.readFile( __dirname + "/" + "db/users.json", 'utf8', function (err, data) {
@@ -17,22 +19,43 @@ var usersRepository = {
 	}
 };
 
-app.get('/users', function (req, res) {
-	usersRepository.getAll(function (data, err) {
-		res.end(data);
-	});
-})
+//Rest services
+var userRestService = {
+	list: function (req, res) { 
+		usersRepository.getAll(function (data, err) {
+			res.end(data);
+		});
+	},
+	get: function (req, res) { 
+		usersRepository.getById(function (data, err) {
+			res.end(data);
+		}, req.params.id);
+	},
+	post: function (req, res) { 
+		var post_data = req.body;
+		console.log(post_data);
+  		res.end(post_data);
+	},
+	put: function (req, res) { 
+		res.send('put'); 
+	},
+	delete: function (req, res) { 
+		res.send('delete'); 
+	}
+};
 
-app.get('/users/:id', function (req, res) {
-	usersRepository.getById(function (data, err) {
-		res.end(data);
-	}, req.params.id);
-})
+//Route configuration
+app.get('/users', userRestService.list);
+app.get('/users/:id', userRestService.get);
+app.post('/users', userRestService.post);
+app.put('/users', userRestService.put);
+app.delete('/users', userRestService.delete);
 
-app.get('/users/a', function (req, res) {
-	res.send('hello from a!');
-})
+//Aplication config
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//Application start
 var server = app.listen(8081, function () {
 	var host = server.address().address
 	var port = server.address().port
